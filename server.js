73d411cd76b8f8802c1e2929af81823b3b5dc07f
misticG <tuy-vi.huynh@medieinstitutet.se>
2019-11-18 20:13:15 +0100

    cleaning up

--- server.js
+++ server.js
@@ -5,15 +5,13 @@ const port = 3000
 
 const apiKey = '4fb12825b7msh51f653b00d3a3e4p134a2ajsn85857b79fc18'
 const url = 'https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random'
-//const hostUrl = 'matchilling-chuck-norris-jokes-v1.p.rapidapi.com'
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('public'))
 
 app.get('/api/chuckieJoke', (req, res) => {
     fetch(url, {
         method: 'GET',
         headers: {
-        //'x-rapidapi-host' : hostUrl,
         'x-rapidapi-key' : apiKey,
         'accept' : 'application/json'
         }
