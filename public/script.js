388a37fba0b35043b7b7751a9935b380806d81cd
misticG <tuy-vi.huynh@medieinstitutet.se>
2019-11-18 20:01:36 +0100

    chuckieQuote fetch get client

---
+++ public/script.js
@@ -0,0 +1,24 @@
+function initSite() {
+
+}
+
+function getChuckieQuote() {
+    fetch('http://localhost:3000/api/chuckieJoke', {
+        method: 'GET'
+    }).then(res => res.json()
+    ).then(data => {
+        console.log(data.value)
+    }).catch(err => console.error(err))
+}
+
+function makeRequest() {
+
+}
+
+function printChuckieInfo() {
+
+}
+
+function printMovieInfo() {
+
+}
