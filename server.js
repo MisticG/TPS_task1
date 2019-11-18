ef9ffe212347c3cab462f0571bb2828dff574601
misticG <tuy-vi.huynh@medieinstitutet.se>
2019-11-18 18:49:09 +0100

    small fix

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console-log('App listening on port ${port}'))
+app.listen(port, () => console.log(`App listening on port ${port}`))
